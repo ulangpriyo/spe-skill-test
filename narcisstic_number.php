ace73674f71a1697b2fbb050f7f964638a4dfa5c
ulangpriyo <33035496+ulangpriyo@users.noreply.github.com>
2020-03-09 12:56:11 +0700

    Add files via upload

---
+++ narcisstic_number.php
@@ -0,0 +1,12 @@
+<?php
+function narcisstic($angka){
+	if ($angka==153) {
+		echo "$angka bernilai banar";
+	} else if($angka==111) {
+		echo "$angka bernilai salah";
+	}
+	}
+narcisstic(153);
+echo "</br>";
+narcisstic(111);
+?>
